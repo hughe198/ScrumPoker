5aaf7d2591145d94f7436807bc9b0f28b782564a
Hugh Ellison <hughellison@gmail.com>
2024-02-11 16:58:45 +0000

    Update, remove dependencies from Remote

---
+++ ScrumPoker/scrumpoker_api/app/api/models/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+# Define all your models (User, Session, Story, Vote) here
+# class User(Base):
+#     # ...
+
+# class Session(Base):
+#     # ...
+
+# class Story(Base):
+#     # ...
+
+# class Vote(Base):
+#     # ...
